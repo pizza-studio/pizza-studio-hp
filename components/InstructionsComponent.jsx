9be38bf00e3a30889754c4df469509087c50f3a4
Bill-Haku <battleandwar@hotmail.com>
2023-07-18 16:29:04 +0800

    update text

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -16,7 +16,7 @@ export default function InstructionsComponent() {
 			<div className={styles.buttons_container}>
 				<a
 					target={"_blank"}
-					href={"https://createweb3dapp.alchemy.com/#components"}
+					href={"https://gi.pizzastudio.org"}
 				>
 					<div className={styles.button}>
 						{/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
@@ -25,7 +25,7 @@ export default function InstructionsComponent() {
 				</a>
 				<a
 					target={"_blank"}
-					href={"https://createweb3dapp.alchemy.com/#templates"}
+					href={"https://hsr.pizzastudio.org"}
 				>
 					<div className={styles.button}>
 						{/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
@@ -34,27 +34,27 @@ export default function InstructionsComponent() {
 				</a>
 				<a
 					target={"_blank"}
-					href={"https://docs.alchemy.com/docs/create-web3-dapp"}
+					href={"https://herta.pizzastudio.org"}
 				>
 					<div className={styles.button}>
-						<img
+						{/* <img
 							src="https://static.alchemyapi.io/images/cw3d/Icon%20Large/file-eye-01-l.svg"
 							width={"20px"}
 							height={"20px"}
-						/>
+						/> */}
 						<p>Herta Terminal</p>
 					</div>
 				</a>
 			</div>
 			<div className={styles.footer}>
-				<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+				{/* <a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
 					<img
 						id="badge-button"
 						style={{ width: "240px", height: "53px" }}
 						src="https://static.alchemyapi.io/images/marketing/badgeLight.png"
 						alt="Alchemy Supercharged"
 					/>
-				</a>
+				</a> */}
 				<div className={styles.icons_container}>
 					<div>
 						<a
--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -1,12 +1,10 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-				<img className={styles.alchemy_logo} src="/cw3d-logo.png"></img>
+			<a href="https://pizzastudio.org" target={"_blank"}>
+				Pizza Studio
 			</a>
-			<ConnectButton></ConnectButton>
 		</nav>
 	);
 }
